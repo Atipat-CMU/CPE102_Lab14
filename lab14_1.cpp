ee540e848dc564a9c6fd8d8565e82d284caf1f79
AtipatD <atipat_d@cmu.ac.th>
2021-12-28 16:35:00 +0700

    lab14_1.cpp completed

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,37 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[], int N, double B[]){
+    double mean = 0;
+    for (int i = 0; i < N; i++){
+        mean += A[i];
+    }
+    B[0] = mean/N;
+
+    double std = 0;
+    for (int i = 0; i < N; i++){
+        std += pow(A[i] - (mean/N),2);
+    }
+    B[1] = sqrt(std/N);
+
+    double geo = 1;
+    for (int i = 0; i < N; i++){
+        geo *= A[i];
+    }
+    B[2] = pow(geo,1./N);
+
+    double hamon = 0;
+    for (int i = 0; i < N; i++){
+        hamon += 1./A[i];
+    }
+    B[3] = N/hamon;
+
+    double max = A[0], min = A[0];
+    for (int i = 1; i < N; i++){
+        if(max < A[i]) max = A[i];
+        if(min > A[i]) min = A[i];
+    }
+    B[4] = max;
+    B[5] = min;
+}
